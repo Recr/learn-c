e67c3e8e1b8aba03d7af3f751df3b160084882e5
Recr <126016150+Recr@users.noreply.github.com>
2023-04-21 00:03:31 -0300

    equation v1.1

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 int main()
 {
 
-    float a, b, c, x1, x2; 
+    float a, b, c, delta, x1, x2; 
 
     printf("This program returns the values of x of a quadratic equation.\n");
-    sleep(2);
-    printf("Note that if the value of \'a\' is equal to 0, it's not a quadratic equation.\n");
-    sleep(2);
+    sleep(3);
+    printf("Note that if the value of \'a\' is equal to 0, it's not a quadratic equation\n");
+    sleep(3);
 
     printf("Insert the value of \'a\'\n");
     scanf("%f", &a);
@@ -20,10 +21,34 @@ int main()
 
     if(a != 0)
      {
+        delta = b*b - (4 * a * c);
 
+        if(delta != 0)
+         {
+            x1 = (- b + sqrt(delta))/ (2*a);
+            x2 = (- b - sqrt(delta))/ (2*a);
+
+            if(x1 == x2)
+             {
+                printf("There's one root for this equation.\n");
+                sleep(2);
+                printf("The value of x is: %f \n", x1);
+             }
+            else
+             {
+                printf("There's two roots for this equation.\n");
+                printf("The value of x1 is %f and the value of x2 is %f", x1, x2);
+             } 
+         }
+        else
+         {
+            printf("There's no value for x in the set of real numbers");
+         } 
      }
     else
      {
         printf("This is not a quadratic equation");
      } 
+
+  return 0;
 }
