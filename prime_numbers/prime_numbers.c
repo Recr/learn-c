2dc3d3b04b2f157dcd72f692a833a812f0172350
Recr <126016150+Recr@users.noreply.github.com>
2023-04-25 11:01:45 -0300

    Primes

--- prime_numbers/prime_numbers.c
+++ prime_numbers/prime_numbers.c
@@ -2,14 +2,43 @@
 
 int main(){
 
- int x;
- x = 1;
-
- while(x < 101)
+ int x, y, z = 1, w = 2;
+ printf("Type the range to verify the primes: ");
+ scanf("%i", &x);
+ if(x < 2)
   {
-    printf("%i\n", x);
-    x++;
-    sleep(1);
+    printf("There's no primes in this range");
   }
+ else
+  {
+    for(w = 2; w < (x + 1); w ++)
+     {
+
+      for(y = 2; y < (w + 1); y ++)
+       {
+        if(w % y == 0)
+         {
+          z++;
+         }
+
+        if(z > 2)
+         {
+          z = 1;
+          break;
+         }
+      
+       }
+
+      if(z == 2)
+       {
+        printf("%i\n", w);
+        z = 1;
+       }
+      
+      
+     }  
+  }
+ 
+getch();
 
 }
