e50a8894b1b944efb3e15c1bd455b41b28789a2a
Recr <126016150+Recr@users.noreply.github.com>
2023-04-25 00:18:50 -0300

    primes

---
+++ prime_numbers/prime_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+ int x;
+ x = 1;
+
+ while(x < 101)
+  {
+    printf("%i\n", x);
+    x++;
+    sleep(1);
+  }
+
+}
--- test.c
+++ test.c
@@ -3,8 +3,13 @@
 
 int main()
 {
+    short a;
     printf("Hello World!\n"); //prints any text.
     sleep(2);
-    printf("This is an introduction to the C Language\a");
+    printf("This is an introduction to the C Language\a\n");
+    printf("char can contain symbols and numbers in a range of -127 to 128\n");
+    a = 32000;
+    printf("%i", a);
+    getchar();
     return 0;
 }
