3ecd47ca60d386b2abcb700b09e413e348b30d95
Recr <126016150+Recr@users.noreply.github.com>
2023-06-13 11:22:17 -0300

    ignore

--- test.c
+++ test.c
@@ -4,12 +4,15 @@
 int main()
 {
     short a;
-    printf("Hello World!\n"); //prints any text.
-    sleep(2);
-    printf("This is an introduction to the C Language\a\n");
-    printf("char can contain symbols and numbers in a range of -127 to 128\n");
-    a = 32000;
-    printf("%i", a);
+    char x;
+ scanf("%c", &x);
+    switch(x)
+ {
+    case 'a':
+    printf("shit");
+ }
     getchar();
     return 0;
+
 }
+
